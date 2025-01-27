390e38517d9597217ee5daf0d58ee49441953a46
Victor <victorchiemelie980@gmail.com>
2025-01-27 02:33:13 +0100

    fixes

--- src/app/components/CoinImage.tsx
+++ src/app/components/CoinImage.tsx
@@ -8,7 +8,7 @@ export default function CoinImage() {
                     <h3 className="text-3xl md:text-5xl font-bold mb-5 text-gray-300">Get Paid Instantly And Easily</h3>
                 </div>
                 <div>
-                <p className="text-gray-400">Once you complete a task, your earnings are ready for transfer and can be quickly moved to your bank account. With GoShare, getting paid is simple and hassle-free. If you're looking for the best ways to make money online, GoShare ensures you receive your rewards as soon as they are processed, making it one of the fastest ways to get paid online today.</p>
+                <p className="text-gray-400">Once you complete a task, your earnings are ready for transfer and can be quickly moved to your bank account. With GoShare, getting paid is simple and hassle-free. If you are looking for the best ways to make money online, GoShare ensures you receive your rewards as soon as they are processed, making it one of the fastest ways to get paid online today.</p>
                 </div>
             </div>
             <div>
